8a0e9c9a0a77fb7ee525eda518dcd27706f9fb9e
Jaiden Chicote <Jaiden.Chicote@CompanionSystems.com.au>
2025-04-07 13:22:12 +1000

    Add email model

---
+++ OpenAndRead_EML_Prototype/EmailModel.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenAndRead_EML_Prototype
+{
+
+    public class EmailModel
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Bcc { get; set; }
+
+        public string Cc { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
